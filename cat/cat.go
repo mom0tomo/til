b683b357b2fde1ca8c87cb57f27cf6fb3378e5b4
momo <mo-watanabe@dip-net.co.jp>
2017-10-12 13:46:07 +0900

    [WIP]cat

--- cat/cat.go
+++ cat/cat.go
@@ -8,31 +8,18 @@ import (
 
 const bufferSize = 2048
 
-func main() {
-	argc := len(os.Args)
-	argv := os.Args
-	if (argc < 2) {
-		fmt.Fprintf(os.Stderr, "%s: ファイルを指定してください\n", argv[0])
-	}
-	for i := 1; i < argc; i++ {
-		doCat(argv[i])
-	}
-	os.Exit(0)
-}
-
-func doCat(path string) {
+func doCat(path string) error {
 	fd, err := os.Open(path)
 	if err != nil {
-		fmt.Errorf("ファイルが開けませんでした%s", fd)
+		return fmt.Errorf("ファイルが開けませんでした%s", fd)
 	}
 	defer fd.Close()
 
 	buf := make([]byte, bufferSize)
 	for {
-		// n, err := fd.Read(fd, buf, unsafe.Sizeof(buf))
 		n, err := fd.Read(buf)
 		if err != nil {
-			fmt.Errorf("ファイルが読み込めませんでした%s", n)
+			return fmt.Errorf("ファイルが読み込めませんでした%s", n)
 		}
 		if n < 0 {
 			fd.Close()
@@ -41,10 +28,32 @@ func doCat(path string) {
 			break
 		}
 		// 怪しい
-		output, _ := fd.Write(buf)
-		if output < 0 {
+		s, err := fd.Write(buf)
+		if s < 0 {
 			fd.Close()
 		}
-		fmt.Println(string(buf[:n]))
+		if err != nil {
+			return fmt.Errorf("標準出力できませんでした%s", s)
+		}
+	}
+	return nil
+}
+
+func run() error {
+	argc := len(os.Args)
+	argv := os.Args
+	if (argc < 2) {
+		return fmt.Errorf("%s: ファイルを指定してください%s")
+	}
+	for i := 1; i < argc; i++ {
+		return doCat(argv[i])
+	}
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(0)
 	}
 }
