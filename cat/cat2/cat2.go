2913d3030977d2265e85f5e231b01a47b335b85a
momo <mo-watanabe@dip-net.co.jp>
2017-10-23 18:01:34 +0900

    add cat2

---
+++ cat/cat2/cat2.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func doCat() error {
+	argv := os.Args
+	argc := len(argv)
+
+	if argc < 2 {
+		return fmt.Errorf("ファイルを指定してください")
+	}
+
+	for i := 1; i < argc; i++ {
+		f, err := os.Open(argv[i])
+		defer f.Close()
+		if err != nil {
+			return fmt.Errorf("ファイルを読み込めませんでした。%s", f)
+		}
+		sc := bufio.NewScanner(f)
+		for sc.Scan() {
+			fmt.Println(sc.Text())
+		}
+		if err := sc.Err(); err != nil {
+			return fmt.Errorf("")
+		}
+	}
+	return nil
+}
+
+func main() {
+	if err := doCat(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
+}
--- head/head1.go
+++ head/head1.go
@@ -1,7 +1,26 @@
+// USAGE: cat sample.txt | ./head1 n
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func do_head() {
+
+}
+
+func run() {
+	argv := os.Args
+	argc := len(argv)
+	if argc != 2 {
+		return fmt.Erroff(stderr, "引数の数が間違っています。正しい利用法はこちら: %s n\n", argv[0])
+	}
+}
 
 func main() {
-	
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
 }
