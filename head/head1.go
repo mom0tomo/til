0706bfa0da2e011e22da6a0b225d1e22d334df65
momo <mo-watanabe@dip-net.co.jp>
2017-11-06 16:41:21 +0900

    add c files

--- head/head1.go
+++ head/head1.go
@@ -6,16 +6,35 @@ import (
 	"os"
 )
 
-func do_head() {
+func do_head() error {
+	fp, _ := os.Open(os.Args[0])
+	if LineCount(fp) <= 0 {
+		return nil
+	}
+	c := os.Stdin.Read(fp)
+	for c != EOF {
+		switch {
+		case c < 0:
+			return nil
+			os.Exit(1)
+		case c == '\n':
+			LineCount--
+			if LineCount(fp) == 0 {
+				return nil
+			}
+		}
 
+	}
+	return nil
 }
 
-func run() {
+func run() error {
 	argv := os.Args
 	argc := len(argv)
 	if argc != 2 {
 		return fmt.Erroff(stderr, "引数の数が間違っています。正しい利用法はこちら: %s n\n", argv[0])
 	}
+	return nil
 }
 
 func main() {
