433c16bd8fb26c6b8ee3884d9dc28032f35e4886
mom0tomo <momo76w@gmail.com>
2017-11-21 10:06:21 +0900

    add head.go

---
+++ head/head.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func do_head() error {
+	nlines, _ := strconv.Atoi(os.Args[3])
+	if (nlines <= 0) {
+		return nil
+	}
+	sc := bufio.NewScanner()
+	sc.scan()
+	for 
+
+
+}
+
+func run() error {
+	
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
